f0f87828105154fac2ae6e75d571d3f71f18cde9
Zahraa  Jalaa <zahraa.jalaa99@gmail.com>
2019-10-18 22:04:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-function BroadMember(name,homeState,training){
-  this.name = name;
-  this.homeState = homeState;
-  this.training = training;
+function BoardMember(name, homeState, training){
+  this.name = name
+  this.homeState = homeState
+  this.training = training
 }
-  BroadMember.prototype.veto = function(){
-    return 'No, I must disagree'; 
-    
-  };
-  BroadMember.prototype.approve = function(){
-    return 'You can do that!';
-    
-  };
-  BroadMember.prototype.doCharity = function(){
-    return 'I like to help people.'
-    
-  };
-  BroadMember.prototype.releasePressStatement = function(){
-    return 'You will see great things from Scuber.';}
-  BroadMember.prototype.sayHi= function (){
-    return `Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`};
+
+BoardMember.prototype.veto = function(){
+  return 'No, I must disagree'
+}
+
+BoardMember.prototype.approve = function(){
+  return 'You can do that!'
+}
+
+BoardMember.prototype.doCharity = function(){
+  return "I like to help people."
+}
+
+BoardMember.prototype.releasePressStatement = function(){
+  return "You will see great things from Scuber."
+}
+
+BoardMember.prototype.sayHi = function(){
+  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+}
