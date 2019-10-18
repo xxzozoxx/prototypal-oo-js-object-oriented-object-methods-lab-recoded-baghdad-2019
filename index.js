dd53fa7c967f7e04f0592a7af871fe53c4318a02
Zahraa  Jalaa <zahraa.jalaa99@gmail.com>
2019-10-18 22:02:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ function BroadMember (name,homeState,training){
   this.homeState = homeState;
   this.training = training;
 }
-  BroadMember.prototype.veto = function(){return 'No, I must disagree'; }
-  BroadMember.prototype.approve = function(){return 'You can do that!'}
-  BroadMember.prototype.doCharity = function(){return 'I like to help people.'}
-  BroadMember.prototype.releasePressStatement = function(){return 'You will see great things from Scuber.';}
-  BroadMember.prototype.sayHi= function (){return `Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`;}
+  BroadMember.prototype.veto = function(){
+    return 'No, I must disagree'; 
+    
+  }
+  BroadMember.prototype.approve = function(){
+    return 'You can do that!';
+    
+  }
+  BroadMember.prototype.doCharity = function(){
+    return 'I like to help people.'
+    
+  }
+  BroadMember.prototype.releasePressStatement = function(){
+    return 'You will see great things from Scuber.';}
+  BroadMember.prototype.sayHi= function (){
+    return `Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`};
