a3cfbdfac003d0f1b981bb4483c378a937159721
Zahraa  Jalaa <zahraa.jalaa99@gmail.com>
2019-10-18 22:03:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ function BroadMember(name,homeState,training){
   this.name = name;
   this.homeState = homeState;
   this.training = training;
-};
+}
   BroadMember.prototype.veto = function(){
     return 'No, I must disagree'; 
     
-  }
+  };
   BroadMember.prototype.approve = function(){
     return 'You can do that!';
     
-  }
+  };
   BroadMember.prototype.doCharity = function(){
     return 'I like to help people.'
     
-  }
+  };
   BroadMember.prototype.releasePressStatement = function(){
     return 'You will see great things from Scuber.';}
   BroadMember.prototype.sayHi= function (){
